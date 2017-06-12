6438a225ad0ebe01a5e29d01ff9b1425ff12688c
System Administrator <cjwawrzonek@gmail.com>
2017-06-12 01:51:08 -0700

    cleanup

--- emailer.py
+++ emailer.py
@@ -3,24 +3,27 @@ import json
 
 from email.mime.text import MIMEText
 
-def email(to, subject, body):
-	with open('config.pvt') as config_file:
-		config = json.load(config_file)
+""" Quick email function
+parameters:
+	user:	 a gmail username
+	passw: 	 a password to authenticate gmail
+	to: 	 recipient address
+	subject: email subject
+	body: 	 email body text """
+def send_gmail(user, passw, to, subject, body):
+	sent_from = user + '@gmail.com'
 
-	sent_from = config['user']
-
-	email_text = """From: %s  
-	To: %s  
-	Subject: %s
-
-	%s
-	""" % (sent_from, to, subject, body)
+	msg = MIMEText(body)
+	msg['Subject'] = subject
+	msg['From'] = sent_from
+	msg['To'] = to
 
 	try:
 		server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 		server.ehlo()
-		server.login(sent_from, config['key'])
-		server.sendmail(sent_from, to, email_text)
-		server.close()
+		server.login(sent_from, passw)
+		server.sendmail(sent_from, to, msg.as_string())
 	except:
 		pass
+
+	server.close()
